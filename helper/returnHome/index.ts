40ce1b61fdd25c44d96dd554d03cc50e123f4fdc
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-04-13 04:22:47 -0300

    [Bugfix] Removed unused file that was causing the build to break

--- helper/returnHome/index.ts
+++ helper/returnHome/index.ts
@@ -2,8 +2,10 @@ const returnHome = () => {
   try {
     if (global.window.location.pathname === "/") {
       global.window.scrollTo(0, 0);
+    } else if (process.env.NODE_ENV !== "production") {
+      global.window.location.href = `http://${global.window.location.host}`;
     } else {
-      global.window.location.href = `https://${global.window.location.hostname}/`;
+      global.window.location.href = `https://${global.window.location.host}`;
     }
   } catch (error) {
     console.log(error);
