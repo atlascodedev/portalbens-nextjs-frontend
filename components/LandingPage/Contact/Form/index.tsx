7909f9ecf7944ad1054161675af5640188ee5cb2
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-04-14 05:53:32 -0300

    [Feature] Contact form fully validated using yup; form state completely hooked up to formik hook; imported a few helpers, hooks and utility components from previous projects

--- components/LandingPage/Contact/Form/index.tsx
+++ components/LandingPage/Contact/Form/index.tsx
@@ -1,12 +1,41 @@
-import { Button, Grid, InputAdornment, TextField } from "@material-ui/core";
+import {
+  Button,
+  Grid,
+  InputAdornment,
+  TextField,
+  Tooltip,
+} from "@material-ui/core";
 import { AccountCircle, Email, Person, Phone } from "@material-ui/icons";
-import { Form, Formik } from "formik";
+import { Form, Formik, useFormik } from "formik";
 import React from "react";
 import ContactFormLayout from "./styles";
+import * as Yup from "yup";
+import NumberFormat from "react-number-format";
 
 interface Props {}
 
 const ContactForm = (props: Props) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    },
+    enableReinitialize: true,
+    isInitialValid: false,
+    validationSchema: Yup.object({
+      name: Yup.string().required("Este campo é obrigatório"),
+      email: Yup.string()
+        .required("Este campo é obrigatório")
+        .email("É preciso ser um e-mail válido"),
+      phone: Yup.string().required("Este campo é obrigatório"),
+      message: Yup.string().required("Este campo é obrigatório"),
+    }),
+
+    onSubmit: (values, actions) => console.log("ass"),
+  });
+
   return (
     <ContactFormLayout>
       <div style={{ paddingTop: "25px", width: "100%" }}>
@@ -15,7 +44,16 @@ const ContactForm = (props: Props) => {
             <AccountCircle />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Nome" />
+            <TextField
+              name="name"
+              value={formik.values["name"]}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched["name"] && Boolean(formik.errors["name"])}
+              fullWidth
+              label="Nome"
+              helperText={formik.errors["name"]}
+            />
           </Grid>
         </Grid>
       </div>
@@ -26,7 +64,18 @@ const ContactForm = (props: Props) => {
             <Phone />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Telefone" />
+            <NumberFormat
+              customInput={TextField}
+              format={"(##) #-####-####"}
+              value={formik.values["phone"]}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched["phone"] && Boolean(formik.errors["phone"])}
+              helperText={formik.errors["phone"]}
+              name="phone"
+              fullWidth
+              label="Telefone"
+            />
           </Grid>
         </Grid>
       </div>
@@ -37,7 +86,16 @@ const ContactForm = (props: Props) => {
             <Email />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Email" />
+            <TextField
+              name="email"
+              value={formik.values["email"]}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched["email"] && Boolean(formik.errors["email"])}
+              helperText={formik.errors["email"]}
+              fullWidth
+              label="Email"
+            />
           </Grid>
         </Grid>
       </div>
@@ -46,6 +104,14 @@ const ContactForm = (props: Props) => {
         <Grid container spacing={1} justify="center">
           <Grid style={{ width: "100%" }} item>
             <TextField
+              value={formik.values["message"]}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={
+                formik.touched["message"] && Boolean(formik.errors["message"])
+              }
+              helperText={formik.errors["message"]}
+              name="message"
               fullWidth
               label="Mensagem"
               multiline
@@ -65,13 +131,25 @@ const ContactForm = (props: Props) => {
           justifyContent: "center",
         }}
       >
-        <Button
-          style={{ backgroundColor: "#324E3E", color: "#fff" }}
-          size="large"
-          variant="contained"
+        <Tooltip
+          title={
+            formik.isValid
+              ? ""
+              : "Verifique todos os campos antes de prosseguir"
+          }
         >
-          Enviar!
-        </Button>
+          <span>
+            <Button
+              color="primary"
+              size="large"
+              variant="contained"
+              onClick={() => console.log(formik.dirty)}
+              disabled={!formik.isValid}
+            >
+              Enviar!
+            </Button>
+          </span>
+        </Tooltip>
       </div>
     </ContactFormLayout>
   );
---
+++ components/Util/FeedbackDialog/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const FeedbackDialog = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FeedbackDialog
